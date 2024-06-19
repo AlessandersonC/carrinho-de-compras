63c9a2bcda2e0516cac5b719713e3e3a3aefc0e2
Alessanderson Carvalho <alex.carvalhosar@gmail.com>
2024-06-18 22:08:01 -0300

    identificando o produto e a quantidade do mesmo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 function adicionar() {
-
+// recuperar valores: nomedo produto, quantidade e valor
+    let produto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+// calcular o preço total do carrinho
+// adicionar o produto no carrinho
+// atualizar o valor total
 }
 
 function limpar() {
-    
+
 }
