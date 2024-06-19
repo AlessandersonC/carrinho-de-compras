e76dcab01e7059a71306da15bc311b14a9aff525
Alessanderson Carvalho <alex.carvalhosar@gmail.com>
2024-06-18 22:14:13 -0300

    valor total de um mesmo produto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 function adicionar() {
 // recuperar valores: nomedo produto, quantidade e valor
-    let produto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorProduto = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
+    let valorTotal = quantidade.value * valorProduto;
 // calcular o preço total do carrinho
+
 // adicionar o produto no carrinho
 // atualizar o valor total
 }
